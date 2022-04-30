54a8b6696b642a90f898b8d6d61fe2e68046da57
zaaydx <zaid.chbani@usmba.ac.ma>
2022-04-30 08:48:07 +0000

    update tasks

--- App.js
+++ App.js
@@ -1,11 +1,22 @@
-import { StatusBar } from 'expo-status-bar';
+import react from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import Task from './components/Task';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+
+      {/* Today's Tasks */}
+      <View style={styles.tasksWrapper}>
+        <Text style={styles.sectionTitle}>Today's task</Text>
+
+        <View style={styles.items}>
+          {/* This's where the tasks will go*/}
+          <Task text={"hello"} />
+          <Task text={"hello"} />
+        </View>
+      </View>
+
     </View>
   );
 }
@@ -13,8 +24,17 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#D4E1D2',
+  },
+  tasksWrapper: {
+    paddingTop: 80,
+    paddingHorizontal: 20,
+  },
+  sectionTitle: {
+    fontSize: 24,
+    fontWeight: 'bold',
+  },
+  items: {
+    marginTop: 30,
   },
 });
---
+++ components/Task.js
@@ -0,0 +1,49 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+
+export default function Task(props) {
+  return (
+    <View style={styles.item}>
+        <View style={styles.itemLeft}>
+            <View style={styles.square}></View>
+            <Text style={styles.itemText}>{props.text}</Text>
+        </View>
+        <View style={styles.circular}></View>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    item: {
+        padding: 15,
+        borderRadius: 10,
+        backgroundColor: '#FFF',
+        alignItems: 'center',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        marginBottom: 20,
+    },
+    square: {
+        width: 24,
+        height: 24,
+        backgroundColor: '#022AFF',
+        opacity: 0.7,
+        borderRadius: 5,
+        marginRight: 15,
+    },
+    itemLeft: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+    circular: {
+        width: 12,
+        height: 12,
+        borderColor: '#022AFF',
+        borderWidth: 2,
+        borderRadius: 5,
+    },
+    itemText: {
+        maxWidth: '80%',
+    },
+});
